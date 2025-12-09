73fa6723acaef3954f11cbc4bb21f51779b16076
serenebliss0 <semajayi1234@gmail.com>
2025-12-09 08:08:28 +0100

    Add project files for LED Arrays, including code and Fritzing diagram

--- projects/01-basics/led-arrays/led-arrays.ino
+++ projects/01-basics/led-arrays/led-arrays.ino
@@ -1,7 +1,21 @@
+/*
+ * Project Name: LED Arrays
+ * Description: Initialize multiple LEDs using an array
+ * Author: Semire Ajayi
+ * Date: 07-DEC-2025
+ * 
+ * Hardware:
+ * - Arduino board: UNO Rev 3
+ * - 6 LEDs of different colors
+ * - 6 220-ohm resistors
+ * 
+ * Connections:
+ * - LEDs -> Pins 2 to 7
+ */
 
 int leds[] = {2,3,4,5,6,7};
 
-int len = sizeof(leds) / sizeof(leds[0]);
+int len = sizeof(leds) / sizeof(leds[0]); //find the length of the leds array
 
 void setup()
 {
